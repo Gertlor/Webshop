f7f5908100d23c1f861901f0c02a0244487b8bd6
Gerrit Louis <gerritlouis@gmail.com>
2018-01-14 14:13:31 +0100

    layout

--- Webshop/src/app/shared/header/header.component.ts
+++ Webshop/src/app/shared/header/header.component.ts
@@ -4,6 +4,7 @@ import {AuthGuardService} from "../../auth/guard/auth-guard.service";
 import {AuthService} from "../../auth/auth.service";
 import {ProductService} from "../../product/product.service";
 import {Account} from "../../account/account";
+import {CartService} from "../../cart/cart.service";
 
 @Component({
   selector: 'app-header',
@@ -15,7 +16,7 @@ export class HeaderComponent implements OnInit {
   currentAccount:Account = new Account();
   isCurrentAdmin:boolean = false;
 
-  constructor(private router:Router, private authService:AuthService) { }
+  constructor(private router:Router, private authService:AuthService,private cartService:CartService) { }
 
   public logout() {
     this.authService.logout();
