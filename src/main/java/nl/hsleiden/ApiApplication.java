e72bd73f334ec45c731b1a3de4b6c2b2fa02ba20
Gerrit Louis <gerritlouis@gmail.com>
2018-01-15 10:27:47 +0100

    changed ip from gerlobes to pi ip

--- src/main/java/nl/hsleiden/ApiApplication.java
+++ src/main/java/nl/hsleiden/ApiApplication.java
@@ -126,7 +126,7 @@ public class ApiApplication extends Application<ApiConfiguration>
         FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
         filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
         filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
-        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*gerlobes.nl");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*94.209.97.253");
         filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
         filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
         filter.setInitParameter("allowCredentials", "true");
